1a9f86d05623518513ee8f2b200e815cd57e4bcc
Javier Lopez <javierlopm@hotmail.com>
2015-04-06 21:22:18 -0430

    Spinner agregado

---
+++ app/src/main/java/net/ddns/javierlopm/quienlibre/CambioTrimestre.java
@@ -0,0 +1,65 @@
+package net.ddns.javierlopm.quienlibre;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class CambioTrimestre extends Activity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spTrimestre);
+        spinner.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.trimestres, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        setContentView(R.layout.activity_cambio_trimestre);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cambio_trimestre, menu);
+        return true;
+    }
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
--- app/src/main/java/net/ddns/javierlopm/quienlibre/MenuPrincipal.java
+++ app/src/main/java/net/ddns/javierlopm/quienlibre/MenuPrincipal.java
@@ -1,9 +1,11 @@
 package net.ddns.javierlopm.quienlibre;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 
 
@@ -15,12 +17,24 @@ public class MenuPrincipal extends ActionBarActivity {
         setContentView(R.layout.activity_menu_principal);
     }
 
-
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_menu_principal, menu);
         return true;
+    }*/
+
+    //Funcion para que cada boton inicie el activity correspondiente
+    public void cambTrim(View view){
+        Intent intent = new Intent(this, CambioTrimestre.class);
+    }
+    public void agregarPersona(View view){
+
+
+    }
+    public void verDisp(View view){
+
     }
 
     @Override
