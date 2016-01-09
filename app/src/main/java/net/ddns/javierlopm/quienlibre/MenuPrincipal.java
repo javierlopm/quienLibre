7542dbc036696a49fcac82f5145aa562dd286dd4
Javier Lopez <javierlopm@hotmail.com>
2016-01-08 23:16:47 -0430

    Muestra de proxima clase completa

--- app/src/main/java/net/ddns/javierlopm/quienlibre/MenuPrincipal.java
+++ app/src/main/java/net/ddns/javierlopm/quienlibre/MenuPrincipal.java
@@ -5,11 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 
 public class MenuPrincipal extends Activity {
@@ -28,79 +27,29 @@ public class MenuPrincipal extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        //archivoTrimestre.delete(); //Util en caso de reinicio en formato del archivo
-        //actualizarVariables();
         ModeloHorario md = new ModeloHorario(this);
         if (! md.hayHorario()) md.poblar();
         md.hayHorario();
-        BloquePropio bp = new BloquePropio();
-        Log.v(mainTag,bp.minToString());
-    }
 
-    public void actualizarVariables(){
-        archivoTrimestre = new File(getApplicationContext().getFilesDir(),"trimestreActual");
-        archivoTrimestre.delete();
-        trimestreSeleccionado = archivoTrimestre.exists();
+        BloquePropio proximaClase = md.proximoBloque();
+        Log.v(mainTag, proximaClase.toString());
 
-        String buffer;
-        String[] tokens;
+        TextView texto = (TextView)findViewById(R.id.salon);
+        texto.setText(proximaClase.edificio.toString() + proximaClase.salon);
 
-        if(trimestreSeleccionado){
-            try {
-                //Lectura y tokenize
-                Scanner lector = new Scanner(archivoTrimestre);
-                buffer = lector.nextLine();
-                tokens = buffer.split(" ");
+        texto = (TextView)findViewById(R.id.hora);
+        texto.setText(proximaClase.dia.toString() + " hora " + proximaClase.hora);
 
-                //Actualizacion de variables
-                anio = Integer.parseInt(tokens[0]);
-                trimestre = tokens[1];
-
-                //Consulta para ver si tengo amigos :D y un horario
-//                ModeloHorario modeloHorario = new ModeloHorario(this);
-//                SQLiteDatabase db = modeloHorario.getReadableDatabase();
-
-                String[] argumentos = new String[3];
-                argumentos[0] = trimestre;
-                argumentos[1] = Integer.toString(anio);
-                argumentos[2] = "me";
-
-//                tengoAmigos = DatabaseUtils.longForQuery(
-//                                    db,
-//                                    "SELECT COUNT(*) " +
-//                                            "FROM horarios " +
-//                                                "WHERE trimestre=? AND " +
-//                                                "anio=? AND " +
-//                                                "nombre!=?",
-//                                    argumentos) > 0L;
-//
-//                tengoHorario = DatabaseUtils.longForQuery(
-//                                    db,
-//                                    "SELECT COUNT(*) " +
-//                                            "FROM horarios " +
-//                                            "WHERE trimestre=? AND " +
-//                                            "anio=? AND " +
-//                                            "nombre=?",
-//                                    argumentos) > 0L;
-
-//                db.close();
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        } else {
-            tengoAmigos  = false;
-            tengoHorario = false;
-        }
-
+        texto = (TextView)findViewById(R.id.asignatura);
+        texto.setText(proximaClase.materia);
 
     }
+
 
 
     @Override
     public void onResume(){
         super.onResume();
-//        actualizarVariables();
     }
 
 
@@ -123,7 +72,7 @@ public class MenuPrincipal extends Activity {
         startActivity(intent);
     }
     public void verDisp(View view){
-        actualizarVariables();
+        //actualizarVariables();
         if (trimestreSeleccionado && tengoHorario && tengoAmigos){
             //Si to_do marcha bien mostrar horario con disponibilidad de amigos
             Intent intent = new Intent(this, VisualizarQuienLibre.class);
@@ -145,20 +94,4 @@ public class MenuPrincipal extends Activity {
 
     }
 
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
